58c21a4f5ee75c35f8e01d9027ba20553a5612e9
Sphax <itssphax123@gmail.com>
2024-05-28 02:01:56 +0000

    12. Let's create a report object

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const employeesObject = {};
-  employeesObject[departmentName] = employees;
-  return employeesObject;
+  return {
+    [departmentName]: [
+      ...employees,
+    ],
+  };
 }
