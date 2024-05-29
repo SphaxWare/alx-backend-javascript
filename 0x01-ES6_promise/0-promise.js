69b571f2da8ff52bfa58ef96dc47aaaa0c2bb1c9
Sphax <itssphax123@gmail.com>
2024-05-29 20:49:26 +0000

    0. Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,5 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
+    // Placeholder
+  });
+}
---
+++ 0x01-ES6_promise/README.md
@@ -0,0 +1,1 @@
+this is readme for 0x01-ES6_promise
