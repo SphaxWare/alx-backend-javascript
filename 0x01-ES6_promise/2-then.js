5fa02b9d2f2d84aab54b507c499f4d62e0365ae6
Sphax <itssphax123@gmail.com>
2024-05-29 21:47:10 +0000

    fix 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success ' };
-    })
-    .catch((error) => {
-      return new Error();
-    });
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
