2b96f74051d73c637def85060db33529da7666ca
Sphax <itssphax123@gmail.com>
2024-05-29 21:09:43 +0000

    fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,13 @@
-export function getFullResponseFromAPI(success){
-  let p = new Promise((resolve, reject) => {
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
     if (success) {
       resolve({
         status: 200,
         body: 'Success',
       });
     } else {
-      reject(new Error('The fake API is not working currently'));
+      const err = new Error('The fake API is not working currently');
+      reject(err);
     }
   });
 }
