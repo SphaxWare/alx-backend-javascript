be79cdcd12b965b8a8224c353cb249e0cd8e2c2c
Sphax <itssphax123@gmail.com>
2024-05-30 00:27:13 +0000

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise
-    .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((value) => [{ status: 'success', value }])
-    .catch((error) => [{ status: 'failure', value: error }]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+
+    const [userData, photoData] = await Promise.all([userPromise, photoPromise]);
+
+    return [
+      { status: 'fulfilled', value: userData },
+      { status: 'fulfilled', value: photoData },
+    ];
+  } catch (error) {
+    return [
+      { status: 'rejected', value: error },
+      { status: 'rejected', value: error },
+    ];
+  }
 }
