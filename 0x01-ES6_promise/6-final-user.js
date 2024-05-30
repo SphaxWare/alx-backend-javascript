6ff3b504770c5ddb3036c73c50192b8fdd73318f
Sphax <itssphax123@gmail.com>
2024-05-30 00:47:43 +0000

    fix try 2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const userPromise = signUpUser(firstName, lastName);
-    const photoPromise = uploadPhoto(fileName);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
 
-    const [userData, photoData] = await Promise.all([userPromise, photoPromise]);
-
-    return [
-      { status: 'fulfilled', value: userData },
-      { status: 'fulfilled', value: photoData },
-    ];
-  } catch (error) {
-    return [
-      { status: 'rejected', value: error },
-      { status: 'rejected', value: error },
-    ];
-  }
+  return Promise.all([userPromise, photoPromise])
+    .then(([userData, photoData]) => {
+      return [
+        { status: 'fulfilled', value: userData },
+        { status: 'fulfilled', value: photoData }
+      ];
+    })
+    .catch(error => {
+      return [
+        { status: 'rejected', value: error },
+        { status: 'rejected', value: error }
+      ];
+    });
 }
