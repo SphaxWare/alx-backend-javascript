6ed121c6c381faa3c552499418a49100b565466f
Sphax <itssphax123@gmail.com>
2024-05-30 01:27:22 +0000

    10. Await / Async

---
+++ 0x01-ES6_promise/100-main.js
@@ -0,0 +1,15 @@
+import asyncUploadUser from "./100-await";
+
+const test = async () => {
+    const value = await asyncUploadUser();
+    console.log(value);
+};
+
+test();
+
+bob@dylan:~$ 
+bob@dylan:~$ npm run dev 100-main.js 
+{
+  photo: { status: 200, body: 'photo-profile-1' },
+  user: { firstName: 'Guillaume', lastName: 'Salva' }
+}
