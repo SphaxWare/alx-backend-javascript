4ed8d3d7a7df6a57fdb43f47f68004d3436e8b2a
Sphax <itssphax123@gmail.com>
2024-06-04 18:43:02 +0000

    fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set) || typeof startString !== 'string') return ('');
+  if (startString === '' || typeof startString !== 'string') return '';
   const result = [];
 
   set.forEach((value) => {
     if (typeof value === 'string' && value.startsWith(startString)) {
-      result.push(value.slice(startString.lenght));
+      result.push(value.slice(startString.length));
     }
   });
   return result.join('-');
