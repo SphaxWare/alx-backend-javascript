8cdf878d738ebe7c6910b02829e15bbf2256a250
Sphax <itssphax123@gmail.com>
2024-06-04 02:16:28 +0000

    4. Combine

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(list, city, newGrades) {
+  return list
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
