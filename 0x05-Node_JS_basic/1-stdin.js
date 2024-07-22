97f69111880b0bcb475d3c6213493456b3b5df69
Sphax <itssphax123@gmail.com>
2024-07-22 11:29:43 -0700

    1. Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,9 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
