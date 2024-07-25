7efffcbb10fa2f0be4e6ea27a4f12b0a81e466c8
Sphax <itssphax123@gmail.com>
2024-07-25 10:58:01 -0700

    fix added middleware

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
