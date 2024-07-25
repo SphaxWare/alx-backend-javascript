3bb91723ca4d0290f811814903a671f35b2ac2d6
Sphax <itssphax123@gmail.com>
2024-07-25 10:34:32 -0700

    fix

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,24 +4,10 @@ const expect = chai.expect;
 const app = require('./api');
 
 describe('Index page', function() {
-  before(function(done) {
-    this.server = app.listen(7865, done);
-  });
-
-  after(function(done) {
-    this.server.close(done);
-  });
-
-  it('should return status code 200', function(done) {
+  it('should return correct output', function(done) {
     request.get('http://localhost:7865', function(error, response, body) {
       expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return the correct message', function(done) {
-    request.get('http://localhost:7865', function(error, response, body) {
-      expect(body).to.equal('Welcome to the payment system');
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
