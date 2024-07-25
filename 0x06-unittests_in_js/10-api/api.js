e696642a659a9b20c8df4d617836aebaa1e0a630
Sphax <itssphax123@gmail.com>
2024-07-25 10:52:28 -0700

    10. Deep equality & Post integration testing

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const { id } = req.params;
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.get('/available_payments', (req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false }
+  });
+});
+
+app.post('/login', (req, res) => {
+  let user = req.body.userName;
+  res.send(`Welcome ${user}`);
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
